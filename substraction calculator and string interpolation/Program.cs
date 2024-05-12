1117db690821a5c0995fddd5280b84e94786fb32
Bibhot1 <bndazbvu.me12@gmail.com>
2024-05-13 05:27:11 +1200

    Add project files.

---
+++ substraction calculator and string interpolation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your first number");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your second number");
+double num2 = double.Parse(Console.ReadLine()); 
+double result = num1 - num2;
+Console.WriteLine($"The value of {num1} - {num2} is {result}");
